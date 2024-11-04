3e24b815f571d3ab167f789309942332e1b00715
Shim Seongbo <107231837+ShimSeongbo@users.noreply.github.com>
2024-11-05 02:14:08 +0900

    feature: 수강생들의 상태 변경 구현
    - Student에 상태 변경 로직 추가
    - StudentService에 상태 변경 로직 추가

--- src/main/java/org/fastcampus/student_management/application/student/StudentService.java
+++ src/main/java/org/fastcampus/student_management/application/student/StudentService.java
@@ -24,9 +24,17 @@ public class StudentService {
 
   public void activateStudent(String name) {
     // TODO: 과제 구현 부분
+    Student student = studentRepository.findByName(name)
+        .orElseThrow(() -> new IllegalArgumentException("해당하는 학생이 없습니다."));
+    student.activate();
+    studentRepository.save(student);
   }
 
   public void deactivateStudent(String name) {
     // TODO: 과제 구현 부분
+    Student student = studentRepository.findByName(name)
+        .orElseThrow(() -> new IllegalArgumentException("해당하는 학생이 없습니다."));
+    student.deactivate();
+    studentRepository.save(student);
   }
 }
